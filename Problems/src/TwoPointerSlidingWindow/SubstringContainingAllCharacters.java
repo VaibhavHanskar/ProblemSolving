caf8e2593cfd5e650f7fc8e2f49c0077d33bc155
vaibhav_hanskar <vaibhav_hanskar@epam.com>
2025-07-31 17:31:03 +0530

    Two pointers

--- Problems/src/TwoPointerSlidingWindow/SubstringContainingAllCharacters.java
+++ Problems/src/TwoPointerSlidingWindow/SubstringContainingAllCharacters.java
@@ -1,4 +1,22 @@
 package TwoPointerSlidingWindow;
+//https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/
+
 
 public class SubstringContainingAllCharacters {
+    public int numberOfSubstrings(String s) {
+        int ans = 0;
+        int[] charArr = new int[3];
+        int left  = 0;
+        int right = 0;
+        while(right < s.length()){
+
+
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        SubstringContainingAllCharacters obj = new SubstringContainingAllCharacters();
+        String s  = "abcabc";
+        System.out.println(obj.numberOfSubstrings(s));
+    }
 }
